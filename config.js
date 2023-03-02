8fd1286b7a3708e828f3146352e2133507a83681
PhantomByte1337 <126546797+PhantomByte1337@users.noreply.github.com>
2023-03-02 09:08:10 +0530

    FLAG-IS-HERE-LOL!!

---
+++ config.js
@@ -0,0 +1,23 @@
+const settings = {
+	block_tor:false,
+	block_proxy:false,
+	block_anonymous:false,
+	port:80,
+	token_expire:'1800s', // 30M
+	offline_shells:true,
+	listeners_ports:[443,1337], 
+	http_reverse_listeners:8080,
+	colors:{
+		shell_list_backgound_color:"black",
+		index_background:"#333333",
+		terminal_color:"white",
+		terminal_background_color:"black",
+	}
+
+};
+
+
+module.exports = {settings};
+ipdata_APIKEY=your_ipdata_apikey
+TOKEN_SECRET=069c352eb55d552f26cd8658f2c864a32058e3067d3c2106319015c0b9b616f6f67ae08a18af8824c7eac2ce341b67fb11df08f9acb51de7483bfb5c8a084a31
+flag=thiran{NOOOO!_You_Got_Me}
